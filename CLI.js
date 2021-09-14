f06ccc2d838c4ff8c09fc5b52da5f695fb22284d
Ashish Prasad <56514722+1-Mirage@users.noreply.github.com>
2021-09-14 22:46:53 +0530

    Adding CLI.js file

---
+++ CLI.js
@@ -0,0 +1,49 @@
+var readlineSync = require('readline-sync');
+var userName = readlineSync.question('May I have your name? ');
+console.log(`Welocme ${userName}`);
+ 
+let score=0;
+function play(question,answer)
+{
+ let userAnswer=readlineSync.question(question);
+ if(userAnswer===answer)
+ {
+   score++;
+   console.log("Correct");
+  
+ }
+ else
+ {
+   console.log("Wrong");
+ }
+ console.log(`Score ${score}`);
+ console.log("------------------")
+}
+ 
+const questionslist=[{
+ question:"What is the decimal equivalent of the binary number 10111? ",
+ answer:"23"
+},
+{
+ question:"What size is twice the size of A4 paper? ",
+ answer:"A3"
+},
+{
+ question:"How many bytes does 4 kilobytes represent? ",
+ answer:"4096"
+},
+{
+ question:"The four bits are called? ",
+ answer:" Nibble"
+},
+{
+ question:"binary value for the decimal value 44? ",
+ answer:" 101100"
+}];
+ 
+for(let i=0;i<questionslist.length;i++)
+{
+ play(questionslist[i].question,questionslist[i].answer);
+}
+console.log("Quiz Complete");
+console.log(`Your Total Score is ${score}`);
