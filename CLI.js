2d6d42e43c62bb20df1f6a41b9532455a2faa7cb
Ashish Prasad <56514722+1-Mirage@users.noreply.github.com>
2021-10-21 18:55:53 +0530

    Update CLI.js with chalk

--- CLI.js
+++ CLI.js
@@ -1,49 +1,51 @@
 var readlineSync = require('readline-sync');
+const chalk = require('chalk');
 var userName = readlineSync.question('May I have your name? ');
-console.log(`Welocme ${userName}`);
- 
+console.log(`Welcome ${userName}`);
+
 let score=0;
 function play(question,answer)
 {
- let userAnswer=readlineSync.question(question);
- if(userAnswer===answer)
- {
-   score++;
-   console.log("Correct");
-  
- }
- else
- {
-   console.log("Wrong");
- }
- console.log(`Score ${score}`);
- console.log("------------------")
+  let userAnswer=readlineSync.question(question);
+  if(userAnswer===answer)
+  {
+    score++;
+    console.log(chalk.green('Correct'));
+    
+  }
+  else
+  {
+    console.log(chalk.red('Wrong'));
+  }
+  console.log(chalk.blue(`Score ${score}`));
+  console.log("------------------")
 }
- 
+
 const questionslist=[{
- question:"What is the decimal equivalent of the binary number 10111? ",
- answer:"23"
+  question:"What is the decimal equivalent of the binary number 10111? ",
+  answer:"23"
 },
 {
- question:"What size is twice the size of A4 paper? ",
- answer:"A3"
+  question:"What size is twice the size of A4 paper? ",
+  answer:"A3"
 },
 {
- question:"How many bytes does 4 kilobytes represent? ",
- answer:"4096"
+  question:"How many bytes does 4 kilobytes represent? ",
+  answer:"4096"
 },
 {
- question:"The four bits are called? ",
- answer:" Nibble"
+  question:"The four bits are called? ",
+  answer:" Nibble"
 },
 {
- question:"binary value for the decimal value 44? ",
- answer:" 101100"
+  question:"binary value for the decimal value 44? ",
+  answer:" 101100"
 }];
- 
+
 for(let i=0;i<questionslist.length;i++)
 {
- play(questionslist[i].question,questionslist[i].answer);
+  play(questionslist[i].question,questionslist[i].answer);
 }
 console.log("Quiz Complete");
-console.log(`Your Total Score is ${score}`);
+console.log(chalk.yellow(`Your Total Score is ${score}`));
+
